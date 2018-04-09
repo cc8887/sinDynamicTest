9176117789ecfa40288195f6b7b344c3a3fdec9d
cc8887 <qq727626166@Gmail.com>
2018-04-09 11:22:25 +0800

    修改了一些bug

--- sinDynamicTest/sinDynamicTest.f90
+++ sinDynamicTest/sinDynamicTest.f90
@@ -1,27 +1,3 @@
-!  sinDynamicTest.f90 
-!
-!  FUNCTIONS:
-!  sinDynamicTest - Entry point of console application.
-!
-
-!****************************************************************************
-!
-!  PROGRAM: sinDynamicTest
-!
-!  PURPOSE:  Entry point for the console application.
-!
-!****************************************************************************
-
-    program sinDynamicTest
-
-    implicit none
-
-    ! Variables
-
-    ! Body of sinDynamicTest
-    print *, 'Hello World'
-
-    end program sinDynamicTest
 
 program cable
 !------------------------------------------------------------------------------
